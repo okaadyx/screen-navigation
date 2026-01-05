36f22d92e5e7e16467a24b69022d52bbce1ec024
Aajad Yadav <develop.contact.help@gmail.com>
2026-01-05 16:12:24 +0530

    Remove commented-out tabBarButton reference in TabLayout

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -28,7 +28,6 @@ export default function TabLayout() {
             <Ionicons name="menu" size={24} color={"white"} />
           </Pressable>
         ),
-        // tabBarButton: HapticTab,
       }}
     >
       <Tabs.Screen
